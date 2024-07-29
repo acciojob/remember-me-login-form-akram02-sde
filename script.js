8a50251d834868e6a9cd359829e05371d0086454
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-07-29 23:07:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+if (localStorage.getItem('username') !== null) {
+	existing.style.display = "block"
+    existing.addEventListener('click', () => {
+        alert(`Logged in as ${localStorage.getItem('username')}`);
+    });
+}
+
+const submit = document.getElementById('submit'); 
+const checkbox = document.getElementById('checkbox'); 
+
+submit.addEventListener('click', () => {
+    const username = document.getElementById('username').value; 
+    const password = document.getElementById('password').value; 
+
+    if (checkbox.checked) {
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+    } else {
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+    }
+    alert(`Logged in as ${username}`);
+});
